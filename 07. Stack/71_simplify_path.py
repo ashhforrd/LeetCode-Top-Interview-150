6b642d9ecf402a91c32bbe63199f650f159b6acd
HaydarChan <atqiyahaydar15@gmail.com>
2026-09-07 15:09:47 +0700

    feat: add simplify path solution

---
+++ 07. Stack/71_simplify_path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        current = ""
+
+        for c in path + "/":
+            if c == "/":
+                if current == "..":
+                    if stack: stack.pop()
+                elif current != "" and current != ".":
+                    stack.append(current)
+                current = ""
+            else:
+                current += c
+        
+        return "/" + "/".join(stack)
